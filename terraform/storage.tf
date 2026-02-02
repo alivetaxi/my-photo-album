cd5ace899c0c1aea0f1d80da9ec0fd2066c422b8
heng <alivetaxi@gmail.com>
2026-02-02 15:57:13 +0800

    refactor: remove legacy Cloud Function deployment and related resources

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -9,15 +9,3 @@ resource "google_storage_bucket" "photos" {
     enabled = false
   }
 }
-
-resource "google_storage_bucket" "function_source" {
-  name          = var.function_source_bucket_name
-  location      = var.region
-  force_destroy = false
-
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled = true
-  }
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,11 +8,3 @@ variable "region" {
 variable "photo_bucket_name" {
   description = "Bucket for photos, videos and thumbnails"
 }
-variable "function_source_bucket_name" {
-  description = "Bucket for Cloud Function source code"
-}
-
-variable "enable_functions" {
-  type    = bool
-  default = false
-}
