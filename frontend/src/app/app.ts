fefc5e1bba15e41d867633b00e38d40419f25e37
heng <alivetaxi@gmail.com>
2026-02-08 23:46:18 +0800

    refactor: update app layout to use Angular Material toolbar and buttons

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './core/auth/auth.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule
+  ],
   templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
 export class App {
   auth: AuthService = inject(AuthService);
+  router = inject(Router);
 
   login(): void {
     this.auth.login();
